8d8b8a0d586b650ccab1b6780670ba92ee11585a
coder1110111 <ayush.yadav.dev610@gmail.com>
2025-01-06 19:37:59 +0530

    Understanding Dynamic Parameters/Routes

---
+++ models/product.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();  //Will assign a random number, high chance to be unique for small examples
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {   //here id is the product id we are trying to find, whereas 'cb' is a callback function
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);    //find is a default function which is used to search through an array for a particular thing
+      cb(product);
+    })
+  }
+
+};
