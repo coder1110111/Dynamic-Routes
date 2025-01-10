24260775272032f8c620a7cfe896d45b4c5e3a7d
coder1110111 <ayush.yadav.dev610>
2025-01-10 17:34:35 +0530

    Added a cart model and its's functioning, still need to implement cart display page

--- models/cart.js
+++ models/cart.js
@@ -9,14 +9,32 @@ const p = path.join(
 );
 
 module.exports = class Cart {
-    static addProduct(id) {
+    static addProduct(id, productPrice) {
         //Fetch the previous cart
-        fs.readFile(p, (err, fileContent => {
-            if
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0};
+            if(!err) {
+                cart = JSON.parse(fileContent);                
+            }
+
+            //analyze the cart => Find if their are any existing products in it
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+             //Add new Product/ increase Quantity
+            if(existingProduct) {
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            }   else    {
+                updatedProduct = { id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.totalPrice = cart.totalPrice + productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err)=>{
+                console.log(err);
+            })
         })
-        //analyze the cart => Find if their are any existing products in it
-        //Add new Product/ increase Quantity
     }
-
-
 }
