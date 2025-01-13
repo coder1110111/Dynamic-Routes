f85cf5a950c871ade3366e65da09f080806edf8f
coder1110111 <ayush.yadav.dev610>
2025-01-13 17:27:11 +0530

    Delete Functionality

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -17,8 +19,9 @@ const getProductsFromFile = cb => {
   });
 };
 
-module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+module.exports = class Product {    //will be called both for adding product and updating product
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id; //will be null for new product and will hold some value for updated product
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,13 +29,34 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();  //Will assign a random number, high chance to be unique for small examples
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if(this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {   //writeFile always rewrite upon the old content
+          console.log(err);
+        });  
+      } else  {
+        this.id = Math.random().toString();  //Will assign a random number, high chance to be unique for small examples
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
+  }
+
+  static deleteById(id)  {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);  //In here we create a new array which contains all the product except for whre prod.id===id.
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          cart.deleteProduct(id, product.price);
+        }
+      })
+    })
   }
 
   static fetchAll(cb) {
