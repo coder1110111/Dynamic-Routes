7d6350717566600b40f8ed2bea244eaf45a26b1a
coder1110111 <ayush.yadav.dev610>
2025-01-20 19:27:28 +0530

    Converted most methods to sequelize

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,9 +21,42 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});        //Essentially a user created the Product
+User.hasMany(Product);
+
+sequelize
+    //.sync( {force:true} )     //will forcibly drop all the table then recreate them with association as stated
+    .sync() 
+    .then(result => {           //Used to check if there is any user or not, if no user will create a dummy user, only for development will be removed in production
+    //console.log(result);
+        return User.findByPk(1)     //findById does not seem to work or is discontinued, hence from now on use findByPk
+    })
+    .then(user => {
+        if(!user)   {
+            return User.create({name: 'Ayush', email: 'testuser@gmail.com'})
+        }
+        return user;
+    })
+    .then(user => {
+        //console.log(user);
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+// currently on video 18. in Understanding Sequelize
